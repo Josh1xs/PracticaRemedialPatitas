7a37bec70ee8fa64207fd2a823be5855fc3b6954
Juan080708 <20240126@ricaldone.edu.sv>
2026-05-19 11:52:52 -0600

    Subida

--- src/Controllers/mascotasController.js
+++ src/Controllers/mascotasController.js
@@ -73,13 +73,20 @@ mascotasController.getMascotasBySpecie = async (req, res) => {
 mascotasController.getMascotasByAge = async (req,res) => {
     try {
 
-        const {age} = req.body
+        const {min, max} = req.body
 
          const mascota = await mascotasModel.find({
-            age: {}
+            age: {$gte: min, $lte: max}
          })
 
-        
+
+         if(!mascota){
+            return res.status(404).json({message: "Not pets with this range of age "})
+         }
+
+         return res.status(200).json(mascota)
+
+
     } catch (error) {
           console.log("error" + error)
         return res.status(500).json({message: "Internal Server Error"});
@@ -87,5 +94,68 @@ mascotasController.getMascotasByAge = async (req,res) => {
 }
 
 
+
+//Insertar mascota
+
+mascotasController.insertMascota = async (req,res) => {
+
+try {
+       const {name, species,age,owner,status} = req.body
+
+
+    const newMascota = new mascotasModel({
+        name,
+        species,
+        age,
+        owner,
+        status,
+        image: req.file.path,
+        public_id: req.file.filename
+    })
+    await newMascota.save();
+    return res.status(200).json({message: "Pet saved"})
+
+} catch (error) {
+     console.log("error" + error)
+        return res.status(500).json({message: "Internal Server Error"});
+}
+ 
+}
+
+
+//Actualizar mascota 
+
+mascotasController.updateMascota = async (req,res) => {
+    try {
+        
+        const {name, species,age,owner,status} = req.body
+        const mascotaFound = await mascotasModel.findById(req.params.id)
+
+            const updateData = {
+                name, 
+                species,
+                age,
+                owner,
+                status
+
+            }
+
+
+            if(req.file){
+
+                await cloudinary.uploader.destroy(mascotaFound.public_id)
+
+                updateData. = req.file.path;
+                updateData.public_id = req.file.filename;
+
+
+            }
+
+
+    } catch (error) {
+         console.log("error" + error)
+        return res.status(500).json({message: "Internal Server Error"});
+    }
+}
 
 export default mascotasController;
